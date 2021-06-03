679f9ce7e9ec8062ec30bf71c2ca2b827086a608
Diego Melendez <diedu89@gmail.com>
2021-06-02 23:48:40 -0600

    feat: initial commit, test unicode markdown

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const IndexPage = () => (
+  <main>
+    Sourcing markdown recipe (see page at{` `}
+    <Link to="æøå-test">/æøå-test</Link>)
+  </main>
+)
+
+export default IndexPage
